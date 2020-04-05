514726fcc4d941a445c8419bc7bbe84f7971f9a7
omer segev <omer@macbook-pro.mynet>
2020-04-05 15:17:59 +0300

    commit

--- src/components/video-player.vue
+++ src/components/video-player.vue
@@ -2,6 +2,7 @@
   <section class="video-player">
     <iframe :src="video.url" frameborder="0" width="420" height="345"></iframe>
     <p class="video-title">{{video.title}}</p>
+    <p class="video-desc">{{video.desc}}</p>
   </section>
 </template>
 
